36154c0f85a5705b34a54b978de503c3f12bdb0a
seeqgold <seeqrothgold@gmail.com>
2025-02-08 13:06:51 +0100

    new commit

--- backend/index.js
+++ backend/index.js
@@ -18,60 +18,6 @@ app.use('/api/user', userRoute);
  app.listen(3000, ()=>{
     console.log('port is running on port 3000');
  });
-// create user
-//login user
-
-
-  
-//   app.get('/api/user',  async(req,res)=>{
-//     try {
-//         const User = await user.find({});
-//         res.status(200).json(User);
-//     } catch (error) {
-//       res.status(500).json({ message: 'Internal Server Error' });
-        
-//     }
-//   });
-
-//   app.get('/api/user/:id',  async(req,res)=>{
-//     try {
-//         const {id} = req.params
-//         const User = await user.findById(id);
-//         res.status(200).json(User);
-//     } catch (error) {
-//       res.status(500).json({ message: 'Internal Server Error' });
-        
-//     }
-//   });
-
-//   app.put('/api/user/:id',  async(req,res)=>{
-//     try {
-//         const {id} = req.params
-//         const User = await user.findByIdAndUpdate(id, req.body);
-//         if (!User) {
-//            return res.status(404).json({message:'user not found'});
-//         };
-//         const updatedUser = await user.findById(id);
-//         res.status(200).json(updatedUser);
-//     } catch (error) {
-//       res.status(500).json({ message: 'Internal Server Error' });
-        
-//     }
-//   });
-
-//   app.delete('/api/user/:id',  async(req,res)=>{
-//     try {
-//         const {id} = req.params
-//         const User = await user.findByIdAndDelete(id);
-//         if (!User) {
-//            return res.status(404).json({message:'user not found'});
-//         };
-//         res.status(200).json({message: 'user deleted sucessfully'});
-//     } catch (error) {
-//       res.status(500).json({ message: 'Internal Server Error' });
-        
-//     }
-//   });
 
  mongoose.connect(process.env.MONGO_URL).then(()=>{
   console.log('DB connected');
