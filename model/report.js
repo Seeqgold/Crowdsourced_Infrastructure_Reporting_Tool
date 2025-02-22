29b31c5c0d7ea51354021b9270bb04795172d278
Esther <otufaleesther@gmail.com>
2025-02-22 07:08:21 +0100

    changes commit

--- model/report.js
+++ model/report.js
@@ -1,14 +1,15 @@
+const { required } = require('joi');
 const mongoose = require('mongoose');
 
 const reportSchema = new mongoose.Schema({
     location:{type:String, required: true},
     title: {type : String, required: true},
-    reportType:{type:String, required:true,default:"Porthole",enum:["Porthole","Streelight","Others"]},
-    description:{type:String, required: true},
-    severity:{type: String, default: "Minor", enum: ["Minor", "Moderate", "Severe"]},
+    reportType:{type:String, required:false,default:"Porthole",enum:["Porthole","Streelight","Others"]},
+    description:{type:String, required: false},
+    severity:{type: String, default: "Minor", enum: ["Minor", "Moderate", "Severe"], required:false},
     duration:{type:String, default:"LessThanAWeek", enum: ["LessThanAWeek","OneToFourWeeks","OverAMonth"]},
-    imageUrl: { type: String, required: true },
-    phoneNumber:{type:String,required:true},
+    imageUrl: { type: String, required: false },
+    phoneNumber:{type:String,required:false},
     additionalComments:{type:String,required:false}
 });
 const Report = mongoose.model('Report',reportSchema);
--- validations/reportValidation.js
+++ validations/reportValidation.js
@@ -4,12 +4,12 @@ const { title } = require('process');
 const reportSchema = Joi.object({
     location: Joi.string().required(),
     title: Joi.string().required(),
-    reportType:Joi.string().required(),
-    description: Joi.string().required(),
-    severity: Joi.string().required(),
-    duration: Joi.string(),
-    imageUrl: Joi.string().required(),
-    phoneNumber: Joi.string(),
+    reportType:Joi.string().required(false),
+    description: Joi.string().required(false),
+    severity: Joi.string().required(false),
+    duration: Joi.string(false),
+    imageUrl: Joi.string().required(false),
+    phoneNumber: Joi.string(false),
     additionalComments: Joi.string(),
 
 });
