147d9985663b87852389d00586e253f4acc6ca15
seeqgold <seeqrothgold@gmail.com>
2025-03-07 13:12:44 +0100

    ssl error fix

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -97,6 +97,7 @@ const forgotPassword =async (req,res) =>{
     res.json({ message: "Password reset link sent to your email." });
   }
    catch (error) {
+    console.error("Forgot password error:", error);
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
