6777143c3a3fd7c9ce8104d013406eb912f3c320
Esther Orieji <oriejiestherr@gmail.com>
2025-03-17 12:41:58 +0100

    logout button works, removed username in login

--- login.js
+++ login.js
@@ -1,9 +1,8 @@
 document.getElementById("login-form").addEventListener("submit", async (e) => {
   e.preventDefault();
-  const username = document.getElementById("username").value;
+  // const username = document.getElementById("username").value;
   const email = document.getElementById("email1").value;
   const password = document.getElementById("password").value;
-  // const role = "user"
 
   const response = await fetch(
     "https://backend-vercel-f4aqq1leu-csia-s-projects.vercel.app/api/user/login",
@@ -12,7 +11,7 @@ document.getElementById("login-form").addEventListener("submit", async (e) => {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ username, email, password }),
+      body: JSON.stringify({ email, password }),
     }
   );
   const data = await response.json();
